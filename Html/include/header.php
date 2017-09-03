a4bd221c505042b0f3a8b4b3b246217c327c48a7
viniciusgf <vinicius.gf@outlook.com>
2017-09-03 20:49:40 -0300

    Initial Commit
    Header and all initial imports.

---
+++ Html/include/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>IPREF </title>
+	<link rel="stylesheet" href="../css/aplication.css">
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+<body>
+	<header id="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 logo">
+					<a href="index.php">
+						<img class="img-responsive" src="../images/logo.png" alt="IPREF Guarulhos" title="IPREF Guarulhos" />
+					</a>
+				</div>
+				<div class="col-lg-3 busca">
+					<div class="center-block align-busca">
+						<i class="fa fa-search" aria-hidden="true"></i>
+						<input class="form-control" type="search" value="" placeholder="o que você procura?" id="busca-header">
+					</div>
+				</div>
+
+				<div class="col-lg-3 btn-header">
+					<div class="bg-btn-header btn-saude">
+						<img class="btn-icon" src="../images/icon-saude.jpg">
+						<div class="btn-text">
+							<h2>SAÚDE</h2>
+							<p>Lorem ipsum dolor sit amet, peasdasdr asmassa jueo. </p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 btn-header">
+					<div class="bg-btn-header btn-previdencia">
+						<img class="btn-icon" src="../images/icon-previdencia.jpg">
+						<div class="btn-text">
+							<h2>PREVIÊNCIA</h2>
+							<p>Lorem ipsum dolor sit amet, peasdasdr asmassa jueo. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
