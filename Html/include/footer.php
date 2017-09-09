38eb05e8ec80b520bad15c5be0b5ba27920df9fc
GabrielaOliv <gabsoliv10@gmail.com>
2017-09-09 19:06:54 -0300

    footer initial

--- Html/include/footer.php
+++ Html/include/footer.php
@@ -1,14 +1,38 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<meta charset="utf-8">
-	<title>IPREF </title>
-	<link rel="stylesheet" href="../css/aplication.css">
-	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-	<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-</head>
-<body>
-	
+	<footer id="footer">
+		<div class="container">
+			<div class="bg-footer">
+				<div class="row">
+					<div class="col-lg-9 contact-footer text-right">
+						<p>
+							<a href="tel:1124616363">Tel. <b>(11)2461-6363</b> <i class="icon-tel-footer"></i></a>
+						</p>
+						<p>
+							Horário de atendimento: das <b>8h às 16:30</b>
+						</p>
+					</div>
+					<div class="col-lg-3 btn-footer">
+						<div class="bg-btn-footer btn-email-footer">
+							<img class="btn-icon" src="../images/icon-saude.jpg">
+							<div>
+								<h2>ACESSO AO WEBMAIL</h2>
+							</div>
+						</div>
+					</div>
+				</div> 
+
+				<div class="row">
+					<div class="col-lg-4 map-site">
+						<h2>
+							PRESTAÇÃO <span>DE SERVIÇOS</span>
+						</h2>
+					</div>
+					<div class="col-lg-4 map-site"></div>
+					<div class="col-lg-4"></div>
+				</div>
+			</div>
+		</div>
+		
+	</footer>
 
 </body>
+</html>
