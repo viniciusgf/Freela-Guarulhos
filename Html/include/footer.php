ec17d84f915be2448aaaff6c4c82937a39765652
viniciusgf <vinicius.gf@outlook.com>
2017-09-10 14:53:16 -0300

    map-site

--- Html/include/footer.php
+++ Html/include/footer.php
@@ -1,117 +1,128 @@
-	<footer id="footer">
-		<div class="container">
-			<div class="bg-footer">
-				<div class="row">
-					<div class="col-lg-9 text-footer text-right">
-						<p>
-							<a href="tel:1124616363">Tel. <b>(11)2461-6363</b> <i class="icon-tel-footer"></i></a>
-						</p>
-						<p>
-							Horário de atendimento: das <b>8h às 16:30</b>
-						</p>
-					</div>
-					<div class="col-lg-3 btn-footer">
-						<a href="" class="bg-btn-radius btn-blue-light">
-							<i class="icon icon-saude"></i>
-							<div class="btn-text">
-								<h2>SAÚDE</h2>
-								<p>Lorem ipsum dolor sit amet, peasdasdr asmassa jueo. </p>
-							</div>
+<footer id="footer">
+	<div class="container">
+		<div class="bg-footer">
+			<div class="row">
+				<div class="col-lg-9 text-footer text-right">
+					<p>
+						<a href="tel:1124616363">Tel. <b>(11)2461-6363</b> <i class="icon-tel-footer"></i></a>
+					</p>
+					<p>
+						Horário de atendimento: das <b>8h às 16:30</b>
+					</p>
+				</div>
+				<div class="col-lg-3 btn-footer">
+					<a href="" class="bg-btn-radius btn-blue-light">
+						<i class="icon icon-saude"></i>
+						<div class="btn-text">
+							<h2>SAÚDE</h2>
+							<p>Lorem ipsum dolor sit amet, peasdasdr asmassa jueo. </p>
+						</div>
+					</a>
+				</div>
+			</div> 
+
+			<div class="row">
+				<div class="col-lg-4 title-bar map-site">
+					<h2>
+						PRESTAÇÃO <span>DE SERVIÇOS</span>
+					</h2>
+					<ul>
+						<li>
+							<a href="#">
+								- Área do Credenciado
+							</a>
+						</li>
+						<li>
+							<a href="#">
+
+								- Holerite
+							</a>
+						</li>
+						<li>
+							<a href="#">
+
+								- Área do Beneficiário
+							</a>
+						</li>
+						<li>
+							<a href="#">
+
+								- Ouvidoria
+							</a>
+						</li>
+
+						<li class="text-left">
+							<a href="#">
+								- Área do Servidor
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								- Portal da Transparência
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								- Planos de saúde
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								- Acesso Restrito
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-lg-4 title-bar map-site">
+					<h2>
+						INSTITUCIONAL
+					</h2>
+
+
+					<ul class="one-colum-map">
+						<li class="text-left">
+							<a href="#">
+								- Sobre o IPREF
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								- Fale conosco.
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								- Onde Estamos.
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								- Mapa do Site.
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-lg-4 text-footer map-site">
+					<h2>
+						IPREF - Instituto de Previdência dos Funcionários
+						Públicos Municipais de Guarulhos.
+					</h2>
+					<p>
+						<a href="#">
+							Rua do Rosário, 226 - Vila Camargos<br>
+							Guarulhos - São Paulo - 07111-080
 						</a>
-					</div>
-				</div> 
-
-				<div class="row">
-					<div class="col-lg-4 title-bar text-footer map-site">
-						<h2>
-							PRESTAÇÃO <span>DE SERVIÇOS</span>
-						</h2>
-						<ul>
-							<li>
-								<a href="#">
-									- Área do Credenciado
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									
-									- Holerite
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									
-									- Área do Beneficiário
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									
-									- Ouvidoria
-								</a>
-							</li>
-						
-							<li class="text-left">
-								<a href="#">
-									- Área do Servidor
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									- Portal da Transparência
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									- Planos de saúde
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									- Acesso Restrito
-								</a>
-							</li>
-						</ul>
-					</div>
-					<div class="col-lg-4 title-bar text-footer map-site">
-						<h2>
-							INSTITUCIONAL
-						</h2>
-						
-
-						<ul>
-							<li class="text-left">
-								<a href="#">
-									- Sobre o IPREF
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									- Fale conosco.
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									- Onde Estamos.
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									- Mapa do Site.
-								</a>
-							</li>
-						</ul>
-					</div>
-					<div class="col-lg-4 text-footer">
-						<p>
-							IPREF - Instituto de Previdência dos Funcionários
-							Públicos Municipais de Guarulhos.
-						</p>
-					</div>
+					</p>
+					<p>
+						<a href="#">
+							siga a gente no facebook!
+						</a>
+					</p>
 				</div>
 			</div>
 		</div>
-	</footer>
+	</div>
+</footer>
 
 </body>
 </html>
