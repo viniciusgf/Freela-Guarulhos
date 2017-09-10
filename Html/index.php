54f0023a015c1c3585d040aa03af4851f325e34e
GabrielaOliv <gabsoliv10@gmail.com>
2017-09-10 16:02:06 -0300

    home

--- Html/index.php
+++ Html/index.php
@@ -5,7 +5,45 @@ include "include/header.php";
 
 <!-- CONTEUDO -->
 
+<section id="slide">
+	<div class="container">
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="../images/slide/img-1.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="../images/slide/img-1.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="../images/slide/img-1.jpg" alt="Los Angeles">
+    </div>
+
+    
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+	</div>
+</section>
 
 <!-- FOOTER -->
 <?php 
