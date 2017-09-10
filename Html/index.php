3c7beb9dbdbca3e61d92399c694e9c74837aba97
GabrielaOliv <gabsoliv10@gmail.com>
2017-09-10 16:34:24 -0300

    index

--- Html/index.php
+++ Html/index.php
@@ -8,40 +8,36 @@ include "include/header.php";
 <section id="slide">
 	<div class="container">
 		<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner">
-    <div class="item active">
-      <img src="../images/slide/img-1.jpg" alt="Los Angeles">
-    </div>
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="../images/slide/img-1.jpg" alt="Los Angeles">
+					<div class="content-slider">
+						<h1>
+							Planos de Saúde <b>IPREF SAÚDE</b>
+							<span>e SAÚDE PLUS.</span>
+						</h1>
+						<a href="">saiba mais <i class="icon icon-arrow-blue"></i></a>
 
-    <div class="item">
-      <img src="../images/slide/img-1.jpg" alt="Los Angeles">
-    </div>
+					</div>
+				</div>
 
-    <div class="item">
-      <img src="../images/slide/img-1.jpg" alt="Los Angeles">
-    </div>
+				<!-- <div class="item">
+					<img src="../images/slide/img-1.jpg" alt="Los Angeles">
+				</div>
 
-    
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+				<div class="item">
+					<img src="../images/slide/img-1.jpg" alt="Los Angeles">
+				</div> -->
+			</div>
+		</div>
 	</div>
 </section>
 
