e3dff3b030c40bd11e68d2221c6f985961644b64
GabrielaOliv <gabsoliv10@gmail.com>
2017-09-12 22:58:52 -0300

    saude detalhe

--- Html/saude-detalhe.php
+++ Html/saude-detalhe.php
@@ -15,23 +15,115 @@ include "include/header.php";
 
 <section id="info-tab">
 	<div class="tabs tabs-style-linemove">
-					<nav>
-						<ul>
-							<li><a href="#section-linemove-1" ><i class="icon icon-credenciados"></i> <span>Rede Credenciada</span></a></li>
-							<li><a href="#section-linemove-2" ><i class="icon icon-caracteristicas"></i> <span>Características | Cobertura</span></a></li>
-							<li><a href="#section-linemove-3" > <i class="icon icon-legislacao"></i> <span>Legislação</span></a></li>
-							<li><a href="#section-linemove-4" ><i class="icon icon-contribuicao"></i> <span>Constribuição | Custeio</span></a></li>
-							<li><a href="#section-linemove-5" ><i class="icon icon-documentos"></i> <span>Documentos para aderir</span></a></li>
-						</ul>
-					</nav>
-					<div class="content-wrap">
-						<section id="section-linemove-1"><p>1</p></section>
-						<section id="section-linemove-2"><p>2</p></section>
-						<section id="section-linemove-3"><p>3</p></section>
-						<section id="section-linemove-4"><p>4</p></section>
-						<section id="section-linemove-5"><p>5</p></section>
-					</div><!-- /content -->
-				</div><!-- /tabs -->
+		<nav>
+			<ul>
+				<li><a href="#section-linemove-1" ><i class="icon icon-credenciados"></i> <span>Rede Credenciada</span></a></li>
+				<li><a href="#section-linemove-2" ><i class="icon icon-caracteristicas"></i> <span>Características | Cobertura</span></a></li>
+				<li><a href="#section-linemove-3" > <i class="icon icon-legislacao"></i> <span>Legislação</span></a></li>
+				<li><a href="#section-linemove-4" ><i class="icon icon-contribuicao"></i> <span>Constribuição | Custeio</span></a></li>
+				<li><a href="#section-linemove-5" ><i class="icon icon-documentos"></i> <span>Documentos para aderir</span></a></li>
+			</ul>
+		</nav>
+		<div class="content-wrap">
+			<section id="section-linemove-1"><p>2</p></section>
+
+			<section id="section-linemove-2">
+				<div class="info-plano">
+					<div class="title-info">
+						<h2>Sobre o  <span>Plano de saúde</span></h2>
+					</div>
+
+					<div class="text">
+						<p>
+							O <b>IPREF SAÚDE</b> é oferecido aos colaboradores da CPFL e aos seus dependentes. Com cobertura de procedimentos completa e rede credenciada de alta qualidade, 
+							você pode cuidar da saúde com muita tranquilidade.
+						</p>
+
+						<p>
+							Sua empresa custeia parte do seu plano de saúde, que é operado no sistema de coparticipação (não possui mensalidade - você participa apenas 
+							com um percentual, de acordo com seu salário, sobre os procedimentos efetivamente utilizados). O IPREF SAÚDE é um plano de saúde coletivo-empresarial, 
+							administrado pela IPREF - uma operadora de saúde sem fins lucrativos.
+						</p>
+					</div>
+
+					<div class="title-info">
+						<h2>Quem pode  <span>aderir</span></h2>
+					</div>
+
+					<div class="text">
+						<p>
+							O <b>IPREF</b> Saúde está disponível aos colaboradores ativos da CPFL, de acordo com as regras da empresa, e aos seus dependentes. 
+							A adesão deve ser feita com a área de Recursos Humanos de sua empresa.
+						</p>
+
+						<p>
+							Sua empresa custeia parte do seu plano de saúde, que é operado no sistema de coparticipação (não possui mensalidade - você participa apenas 
+							com um percentual, de acordo com seu salário, sobre os procedimentos efetivamente utilizados). O IPREF SAÚDE é um plano de saúde coletivo-empresarial, 
+							administrado pela IPREF - uma operadora de saúde sem fins lucrativos.
+						</p>
+					</div>
+
+					<div class="title-info">
+						<h2>Padrão de  <span>acomodação</span></h2>
+					</div>
+
+					<div class="text">
+						<p>
+							A acomodação do plano <b>IPREF</b> é privativa, ou seja:
+						</p>
+
+						<p class="line">
+							<i class="icon icon-check-blue"></i> Apartamento <b>individual</b>;<br>
+
+							<i class="icon icon-check-blue"></i> Banheiro <b>privativo</b>;<br>
+
+							<i class="icon icon-check-blue"></i>Com direito a acompanhante;<br>
+
+							<i class="icon icon-check-blue"></i> Horário livre para visitas.<br>
+						</p>
+					</div>
+
+
+
+					<div class="title-info">
+						<h2>Procedimentos <span>cobertos</span></h2>
+					</div>
+
+					<div class="text">
+						<p>
+							Além de atender todos os procedimentos exigidos pela ANS (Agência Nacional de Saúde Suplementar), a cobertura do plano de saúde vai além e, mantendo 
+							o objetivo de contribuir para que você tenha uma vida mais digna, oferece coberturas adicionais.
+						</p>
+
+						<p class="alt">
+							Confira as principais (a lista completa pode ser verificada no regulamento do plano de saúde):
+						</p>
+
+						<p class="line">
+							<i class="icon icon-check-blue"></i> Cobertura de todos os procedimentos <b>exigidos para ANS;</b><br>
+
+							<i class="icon icon-check-blue"></i> Além dos transplantes exigidos pela legislação (córnea, rim e autógeno de medula), cobertura também para <b>transplantes dos demais órgãos</b>;<br>
+
+							<i class="icon icon-check-blue"></i>Assistência e internação domiciliar para <b>situações especiais</b>;<br>
+
+							<i class="icon icon-check-blue"></i> Sessões de fonoaudiologia;<br>
+
+							<i class="icon icon-check-blue"></i> Sessões de psicologia;<br>
+
+							<i class="icon icon-check-blue"></i> Sessões de terapia ocupacional;<br>
+
+							<i class="icon icon-check-blue"></i> Sessões de RPG;<br>
+
+							<i class="icon icon-check-blue"></i> Horário livre para visitas.<br>
+						</p>
+					</div>
+				</div>
+			</section>
+			<section id="section-linemove-3"><p>3</p></section>
+			<section id="section-linemove-4"><p>4</p></section>
+			<section id="section-linemove-5"><p>5</p></section>
+		</div><!-- /content -->
+	</div><!-- /tabs -->
 </section>
 
 
