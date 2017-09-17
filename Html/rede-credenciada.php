05bb6e4b5c41a6bab9294b0a32f171fa754b50d3
GabrielaOliv <gabsoliv10@gmail.com>
2017-09-17 12:24:47 -0300

    rede credenciada

---
+++ Html/rede-credenciada.php
@@ -0,0 +1,40 @@
+<!-- HEADER -->
+<?php 
+include "include/header.php";
+?>
+
+<!-- CONTEUDO -->
+
+<section id="breadcrumb">
+	<div class="container">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="#">Página inicial</a></li>
+			<li class="breadcrumb-item active">Sobre o Ipref</li>
+		</ol>
+	</div>
+</section>
+
+<section id="mapa-rede-credenciada">
+	<div class="container">
+		<div class="row">
+
+			<iframe src="http://facgeoipref.facilinformatica.com.br/" width="1240" height="600"></iframe>
+
+		</div>
+	</div>
+</section>
+
+
+
+<!-- FOOTER -->
+<?php 
+include "include/footer.php";
+?>
+
+<!-- JS ALL PAGES -->
+<?php 
+include "include/js.php";
+?>
+
+</body>
+</html>
